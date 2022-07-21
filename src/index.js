6a413b02a90b5e114db0cdaca4e1ccfe31e420e8
shihono <26952997+shihono@users.noreply.github.com>
2022-07-21 20:27:47 +0900

    add prohibitKansuji, prohibitFigures

--- src/index.js
+++ src/index.js
@@ -1,25 +1,39 @@
-import { findKanjiNumbers } from '@geolonia/japanese-numeral'
+import { findKanjiNumbers, kanji2number } from '@geolonia/japanese-numeral'
+
+const defaultOptions = {
+    prohibitKansuji: false,
+    prohibitFigure: false
+};
+
+const regexKansuji = /[一二三四五六七八九十]/g;
+const regexFigures = /\d/g;
 
 export default function(context, options = {}) {
     const {Syntax, RuleError, report, getSource} = context;
+    const prohibitKansuji = options.prohibitKansuji ? options.prohibitKansuji : defaultOptions.prohibitKansuji;
+    const prohibitFigure = options.prohibitFigure ? options.prohibitFigure : defaultOptions.prohibitFigure;
     return {
         [Syntax.Str](node){ // "Str" node
             const text = getSource(node); // Get text
             const numbers = findKanjiNumbers(text);
 
-            function getRuleError(word){
+            function getRuleError(word, message="Found bugs."){
                 const re = new RegExp(word);
                 const indexOfBugs = re.exec(text).index;
-                const ruleError = new RuleError("Found bugs.", {
+                const ruleError = new RuleError(message, {
                     index: indexOfBugs // padding of index
                 });
                 return ruleError
             };
             for(const num of numbers){
                 // judge number
-                const matchNumCount = /^\d{5,}[万億兆]$/.test(num);
-                if(matchNumCount === true){
-                    report(node, getRuleError(num));
+                const kansujiIndex = num.search(regexKansuji);
+                const figureIndex = num.search(regexFigures);
+                if(prohibitKansuji===true&kansujiIndex>=0&kanji2number(num)>10){
+                    report(node, getRuleError(num, "漢数字が含まれています"));
+                }
+                else if(prohibitFigure===true&figureIndex>=0){
+                    report(node, getRuleError(num, "アラビア数字が含まれています"));
                 }
             }
         }      
--- test/index-test.js
+++ test/index-test.js
@@ -11,30 +11,56 @@ tester.run("rule", rule, {
         "1200000",
         "漢数字を含む1億円。",
         "漢数字を含む十二億円。",
+        {
+            text: "一期一会の出会い。二人乗り自転車。",
+            options: {
+                "prohibitKansuji": true,
+            },
+
+        }
     ],
     invalid: [
         // single match
         {
             text: "合計で10000億円になる。",
+            options: {
+                "prohibitFigure": true,
+            },
             errors: [
                 {
-                    message: "Found bugs.",
+                    message: "アラビア数字が含まれています",
                     line: 1,
                     column: 4
+                }
+            ]
+        },
+        {
+            text: "漢数字の十億円。",
+            options: {
+                "prohibitKansuji": true,
+            },
+            errors: [
+                {
+                    message: "漢数字が含まれています",
+                    line: 1,
+                    column: 5
                 }
             ]
         },
         // multiple match
         {
             text: `70000兆。\n\n数字の80000万。`,
+            options: {
+                "prohibitFigure": true,
+            },
             errors: [
                 {
-                    message: "Found bugs.",
+                    message: "アラビア数字が含まれています",
                     line: 1,
                     column: 1
                 },
                 {
-                    message: "Found bugs.",
+                    message: "アラビア数字が含まれています",
                     line: 3,
                     column: 4
                 }
